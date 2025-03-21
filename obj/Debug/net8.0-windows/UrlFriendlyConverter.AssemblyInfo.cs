0b093eeaa3c3698dcffd7c298d6659bb4f4fece9
Mateusz <mateuszcieslik@gmail.com>
2025-03-21 13:33:38 +0100

    btn name change

--- obj/Debug/net8.0-windows/UrlFriendlyConverter.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/UrlFriendlyConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlFriendlyConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc139c8d220ea2e6732f7761dd2ceb595f51878")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlFriendlyConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlFriendlyConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
